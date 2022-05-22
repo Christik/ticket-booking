8f86273357b5543e5215ea6db900e9edf1941efb
Christik <christik.lucid@gmail.com>
2022-05-22 11:25:27 +0300

    Update production folder /dist/

--- src/utilities/index-of-element.js
+++
@@ -1,8 +0,0 @@
-export default (element) => {
-    const parent = element.parentElement;
-    const elements = parent.children;
-    const array = [...elements];
-    const index = array.indexOf(element);
-
-    return index;
-};
